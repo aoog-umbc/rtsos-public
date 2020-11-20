dad289773a85fd1c38c36f90ab486757d287e57f
Pengwang Zhai <pwzhai@umbc.edu>
2020-11-20 10:39:56 -0500

    update python script to prepare input files for pace_simulator

---
+++ test/PACE_Simulator/python_script/pace_simulator_inputfile_prepare.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+def input_writer(filestrbase,ichla,itheta,ipss):
+    fileinput='input_ps_'+filestrbase
+    f = open(fileinput, 'w')
+    f.write("%f" % wndspd + '        #wind speed \n')
+    f.write("%f" % theta0[itheta]+ '       #THETA0 in degrees \n')
+    f.write("%f" % tau550 + '        #TAU550 \n')
+    f.write("%d" % irh+ '        #Relative Humidity IRH=1,5, RH=[0.50,0.70,0.80,0.90,0.95] \n')
+    f.write("%d" % iaerosol+ '        #IAEROSOL=1,20. 1-10 is Shettle and Fenn, 11-20 is Ahmad model \n')
+    f.write("%d" % ocean_case_select + '   #OCEAN_CASE_SELECT, case 0(atmosphere only), case 1 [Chla] parameterization, case 2 [Chla]+Sediment, case 3: seven parameter model\n')
+    f.write("%f" % water_depth_max + '   #water_depth_max \n')
+    f.write("%f" % chla[ichla] + '        #CHLa \n')
+    f.write("%f" % phytoplankton_index_refraction + '        #PHYTOPLANKTON_INDEX_REFRACTION \n')
+    f.write("%f" % phytoplankton_spectral_slope + '        #PHYTOPLANKTON_SPECTRAL_SLOPE\n')
+    f.write("%f" % sediment_index_refraction + '        #SEDIMENT_INDEX_REFRACTION \n')
+    f.write("%f" % sediment_spectral_slope + '        #SEDIMENT_SPECTRAL_SLOPE \n')
+    f.write("%f" % sediment_concentration + '        #SEDIMENT_CONCENTRATION \n')
+    f.write("%f" % adg440 + '        #adg440 (1/m), range: 0.0:2.5 \n')
+    f.write("%f" % bbp660_BackscatterCoeff + '        #bbp660 (1/m), range 0:0.1\n')
+    f.write("%f" % Bp660_BackscatterFraction + '      #Bp660_BackscatterFraction, range 0"0.05 \n')
+    f.write("%f" % Sdg + '        #Sdg exponential spectral slope of dg absorption (1/nm) range: 0.01:0.02 \n')
+    f.write("%f" % Sbp + '        #Sbp power spectral slope of backscattering coefficient (1/nm) range: 0:0.5\n')
+    f.write("%f" % S_Bp + '       #S_Bp power spectral slope of backscattering fraction (1/nm) range: -0.2:0.2 \n')
+    f.write("%d" % ncolinput + '        #NCOLINPUT \n')
+    f.write("%d" % nquadainput + '        #NQUADAINPUT \n')
+    f.write("%d" % nquadoinput + '        #NQUADOINPUT \n')
+    f.write("%d" % MAXMORDINPUT + '        #MAXMORDINPUT \n')
+    f.write("%d" % chla_homogeneity + '        #CHLA_HOMOGENEITY, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_raman_flag + '        #OCEAN_RAMAN_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_fchla_flag + '        #OCEAN_FCHLA_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_fcdom_flag + '        #OCEAN_FCDOM_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ap_select + '              #AP_SELECT, 1 Bricaud LUT, 2 Mixure of pico and micron cells in Ciott et al. 2002 \n')
+    f.write("%d" % monochromatic_flag + '     #monochromatic_flag 1=.true. 0=.false. \n')
+    f.write("%d" % hyspectral_flag + '        #HYSPECTRAL_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % npq_flag + '        #NPQ_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_phmx_one + '        #OCEAN_PHMX_ONE, 1=.true. 0=.false. \n')
+    f.write("%d" % atmos_zero + '        #atmos_zero, 1=.true. 0=.false. \n')
+    f.write("%d" % SUNGLINT_INPUT     + '        #SUNGLINT_INPUT, 0 include sun glint 1 no sun glint \n')
+    f.write("%d" % gas_abs_flag + '        #GAS_ABS_FLAG, 1=.true. 0=.false. \n')
+    f.write("%f" % OZONE_COLUMN + '       #ozone column amount in Dobson Unit \n')
+    f.write("%f" % H2O_COLUMN + '       #water vapor column amount in cm \n')
+    f.write("%f" % pressure_surface + '       # surface pressure in mb \n')
+    f.write("%d" % wv_seg_flag + '        #wv_seg_flag, 0: all; 1: seg1+3only; 2: seg2only; 3 seg1+2+3; 4: seg4only \n')
+    f.write("%f" % AirSensor_Height + '       # AirSensor_Height unit km \n')
+    f.write("%d" % pss_flag[ipss] + '       # pseudospherical flag, 0 turn off; 1: turn on \n')
+    f.write("%s" % atmos_profile_filename +'\n')
+    f.write("%s" % 'output_ps_' + filestrbase+'\n')
+    f.close
+
+wndspd=5.0
+theta0=np.array([30.0, 85.0])
+tau550=0.1
+irh=3
+iaerosol=16
+
+#ocean_case_select=np.array([0, 1, 2, 3])
+ocean_case_select=1
+water_depth_max=200.0
+
+# the following parameters are used in ocean_case_select==2
+# chla, phytoplankton_index_refraction,phytoplankton_spectral_slope,
+#sediment_index_refraction,sediment_spectral_slope,sediment_concentration
+chla=np.array([0.01, 0.03, 0.1, 0.3, 1.0, 3, 10])
+phytoplankton_index_refraction=1.02
+phytoplankton_spectral_slope=3.0
+sediment_index_refraction=1.2
+sediment_spectral_slope=4.0
+sediment_concentration=0.0
+
+# the following parameters are used in ocean_case_select ==3, the seven parameter model
+# chla, adg440, bbp660_BackscatterCoeff,Bp660_BackscatterFraction,Sdg,Sbp,S_Bp
+adg440=1.0                         #adg440 (1/m), range: 0.0:2.5
+bbp660_BackscatterCoeff=0.05       #bbp660 (1/m), range 0:0.1
+Bp660_BackscatterFraction=0.01     #Bp660_BackscatterFraction, range 0"0.05
+Sdg=0.015                          #Sdg exponential spectral slope of dg absorption (1/nm) range: 0.01:0.02
+Sbp=0.3                            #Sbp power spectral slope of backscattering coefficient (1/nm) range: 0:0.5
+S_Bp=0.01                          #S_Bp power spectral slope of backscattering fraction (1/nm) range: -0.2:0.2
+
+ncolinput=20
+nquadainput=40
+nquadoinput=60
+MAXMORDINPUT=20
+
+chla_homogeneity=1
+ocean_raman_flag=1
+ocean_fchla_flag=1
+ocean_fcdom_flag=1
+
+ap_select = 1
+monochromatic_flag=0
+hyspectral_flag=0
+npq_flag=0
+
+ocean_phmx_one=1
+# if true, only use one phase matrix for ocean waters
+atmos_zero=0
+SUNGLINT_INPUT=0
+
+gas_abs_flag=1
+
+wv_seg_flag=0
+AirSensor_Height=2.2
+pss_flag=np.array([0, 1])
+
+# these two values are the reference values caculated from US standard atmosphere 1976.
+OZONE_COLUMN=345.66 # OZONE IN THE WHOLE COLUMN IN DOBSON UNIT
+H2O_COLUMN=1.4387   # WATER VAPOR IN THE WHOLE COLUMN IN CENTIMETERS.
+
+pressure_surface=1013.0 #surface pressure in mb
+
+
+atmos_profile_base='afglus'
+atmos_profile_filename=atmos_profile_base+'.dat'
+
+#possible atmosphere profiles are:
+#afglus.dat
+#afglsw.dat
+#afglss.dat
+#afglmw.dat
+
+###########
+
+for ichla in range(len(chla)):
+	for itheta in range(len(theta0)):
+		for ipss in range(len(pss_flag)):
+			filestrbase='OceanCase%d' % ocean_case_select \
+						+ 'tau550_%05.2f' % tau550       \
+						+'chla%05.2f' % chla[ichla] \
+						+'theta0_%05.2f' %theta0[itheta] \
+						+'pss%d'%pss_flag[ipss]
+			input_writer(filestrbase,ichla,itheta,ipss)
+
+
+
+
+
+
---
+++ test/PACE_Simulator/python_script/synthetic_Holton_sequence_inputfiles.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 11:47:30 2020
+
+@author: kamal
+"""
+
+import numpy as np                                                                                                                                                      
+def input_writer(filestrbase,chla,iaerosol,irh,theta0,tau550):
+    fileinput='input_ps_'+filestrbase
+    f = open(fileinput, 'w')
+    f.write("%f" % wndspd + '        #wind speed \n')
+    f.write("%f" % theta0+ '       #THETA0 in degrees \n')
+    f.write("%f" % tau550+ '        #TAU550 \n')
+    f.write("%d" % irh+ '        #Relative Humidity IRH=1,5, RH=[0.50,0.70,0.80,0.90,0.95] \n')
+    f.write("%d" % iaerosol+ '        #IAEROSOL=1,20. 1-10 is Shettle and Fenn, 11-20 is Ahmad model \n')
+    f.write("%d" % ocean_case_select + '   #OCEAN_CASE_SELECT, case 0(atmosphere only), case 1 [Chla] parameterization, case 2 [Chla]+Sediment, case 3: seven parameter model\n')
+    f.write("%f" % water_depth_max + '   #water_depth_max \n')
+    f.write("%f" % chla+ '        #CHLa \n')
+    f.write("%f" % phytoplankton_index_refraction + '        #PHYTOPLANKTON_INDEX_REFRACTION \n')
+    f.write("%f" % phytoplankton_spectral_slope + '        #PHYTOPLANKTON_SPECTRAL_SLOPE\n')
+    f.write("%f" % sediment_index_refraction + '        #SEDIMENT_INDEX_REFRACTION \n')
+    f.write("%f" % sediment_spectral_slope + '        #SEDIMENT_SPECTRAL_SLOPE \n')
+    f.write("%f" % sediment_concentration + '        #SEDIMENT_CONCENTRATION \n')
+    f.write("%f" % adg440 + '        #adg440 (1/m), range: 0.0:2.5 \n')
+    f.write("%f" % bbp660_BackscatterCoeff + '        #bbp660 (1/m), range 0:0.1\n')
+    f.write("%f" % Bp660_BackscatterFraction + '      #Bp660_BackscatterFraction, range 0"0.05 \n')
+    f.write("%f" % Sdg + '        #Sdg exponential spectral slope of dg absorption (1/nm) range: 0.01:0.02 \n')
+    f.write("%f" % Sbp + '        #Sbp power spectral slope of backscattering coefficient (1/nm) range: 0:0.5\n')
+    f.write("%f" % S_Bp + '       #S_Bp power spectral slope of backscattering fraction (1/nm) range: -0.2:0.2 \n')
+    f.write("%d" % ncolinput + '        #NCOLINPUT \n')
+    f.write("%d" % nquadainput + '        #NQUADAINPUT \n')
+    f.write("%d" % nquadoinput + '        #NQUADOINPUT \n')
+    f.write("%d" % MAXMORDINPUT + '        #MAXMORDINPUT \n')
+    f.write("%d" % chla_homogeneity + '        #CHLA_HOMOGENEITY, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_raman_flag + '        #OCEAN_RAMAN_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_fchla_flag + '        #OCEAN_FCHLA_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_fcdom_flag + '        #OCEAN_FCDOM_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ap_select + '              #AP_SELECT, 1 Bricaud LUT, 2 Mixure of pico and micron cells in Ciott et al. 2002 \n')
+    f.write("%d" % monochromatic_flag + '     #monochromatic_flag 1=.true. 0=.false. \n')
+    f.write("%d" % hyspectral_flag + '        #HYSPECTRAL_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % npq_flag + '        #NPQ_FLAG, 1=.true. 0=.false. \n')
+    f.write("%d" % ocean_phmx_one + '        #OCEAN_PHMX_ONE, 1=.true. 0=.false. \n')
+    f.write("%d" % atmos_zero + '        #atmos_zero, 1=.true. 0=.false. \n')
+    f.write("%d" % SUNGLINT_INPUT + '        #SUNGLINT_INPUT, 0 include sun glint 1 no sun glint \n')
+    f.write("%d" % gas_abs_flag + '        #GAS_ABS_FLAG, 1=.true. 0=.false. \n')
+    f.write("%f" % OZONE_COLUMN + '       #ozone column amount in Dobson Unit \n')
+    f.write("%f" % H2O_COLUMN + '       #water vapor column amount in cm \n')
+    f.write("%f" % pressure_surface + '       # surface pressure in mb \n')
+    f.write("%d" % wv_seg_flag + '        #wv_seg_flag, 0: all; 1: seg1+3only; 2: seg2only; 3 seg1+2+3; 4: seg4only \n')
+    f.write("%f" % AirSensor_Height + '       # AirSensor_Height unit km \n')
+    f.write("%d" % pss_flag + '       # pseudospherical flag, 0 turn off; 1: turn on \n')
+    f.write("%s" % atmos_profile_filename +'\n')
+    f.write("%s" % 'output_ps_' + filestrbase+'\n')
+    f.close
+
+wndspd=5.0                                                                                                                                                         
+ocean_case_select=1
+monochromatic_flag=0
+atmos_zero=0
+gas_abs_flag=1
+wv_seg_flag=0
+SUNGLINT_INPUT=0
+
+water_depth_max=200.0
+
+# the following parameters are used in ocean_case_select==2
+# chla, phytoplankton_index_refraction,phytoplankton_spectral_slope,
+#sediment_index_refraction,sediment_spectral_slope,sediment_concentration
+phytoplankton_index_refraction=1.02
+phytoplankton_spectral_slope=3.0
+sediment_index_refraction=1.2
+sediment_spectral_slope=4.0
+sediment_concentration=0.0
+
+# the following parameters are used in ocean_case_select ==3, the seven parameter model
+# chla, adg440, bbp660_BackscatterCoeff,Bp660_BackscatterFraction,Sdg,Sbp,S_Bp
+adg440=1.0                         #adg440 (1/m), range: 0.0:2.5
+bbp660_BackscatterCoeff=0.05       #bbp660 (1/m), range 0:0.1
+Bp660_BackscatterFraction=0.01     #Bp660_BackscatterFraction, range 0"0.05
+Sdg=0.015                          #Sdg exponential spectral slope of dg absorption (1/nm) range: 0.01:0.02
+Sbp=0.3                            #Sbp power spectral slope of backscattering coefficient (1/nm) range: 0:0.5
+S_Bp=0.01                          #S_Bp power spectral slope of backscattering fraction (1/nm) range: -0.2:0.2
+
+ncolinput=20
+nquadainput=40
+nquadoinput=60
+MAXMORDINPUT=20
+
+chla_homogeneity=1
+ocean_raman_flag=1
+ocean_fchla_flag=1
+ocean_fcdom_flag=1
+
+ap_select = 1
+
+hyspectral_flag=0
+npq_flag=0
+
+ocean_phmx_one=1
+# if true, only use one phase matrix for ocean waters
+
+
+
+
+AirSensor_Height=2.2
+pss_flag=1
+
+# these two values are the reference values caculated from US standard atmosphere 1976.
+OZONE_COLUMN=345.66 # OZONE IN THE WHOLE COLUMN IN DOBSON UNIT
+H2O_COLUMN=1.4387   # WATER VAPOR IN THE WHOLE COLUMN IN CENTIMETERS.
+
+pressure_surface=1013.0 #surface pressure in mb
+
+atmos_profile_base='afglus'
+atmos_profile_filename=atmos_profile_base+'.dat'
+
+#possible atmosphere profiles are:
+#afglus.dat
+#afglsw.dat
+#afglss.dat
+#afglmw.dat
+
+###########
+# Now lets generate the 100 terms of halton sequence between 0 and 1 
+dim=5
+n_sample=100
+
+def primes_from_2_to(n):
+    sieve = np.ones(n // 3 + (n % 6 == 2), dtype=np.bool)
+    for i in range(1, int(n ** 0.5) // 3 + 1):
+        if sieve[i]:
+            k = 3 * i + 1 | 1
+            sieve[k * k // 3::2 * k] = False
+            sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = False
+    return np.r_[2, 3, ((3 * np.nonzero(sieve)[0][1:] + 1) | 1)]
+
+
+def van_der_corput(n_sample, base=2):
+    n_sample,base=int(n_sample),int(base)
+    sequence = []
+    for i in range(n_sample):
+        n_th_number, denom = 0., 1.
+        while i > 0:
+            i, remainder = divmod(i, base)
+            denom *= base
+            n_th_number += remainder / denom
+        sequence.append(n_th_number)
+
+    return sequence
+
+
+def halton(dim, n_sample):
+    big_number = 10
+    while 'Not enought primes':
+        base = primes_from_2_to(big_number)[:dim]
+        if len(base) == dim:
+            break
+        big_number += 1000
+
+    # Generate a sample using a Van der Corput sequence per dimension.
+    sample = [van_der_corput(n_sample + 1, dim) for dim in base]
+    sample = np.stack(sample, axis=-1)[1:]
+
+    return sample
+
+hs=halton(dim,n_sample) #this gives the sequence of halton terms stacked in 
+                       #n_sample dimensional array with 5 independent halton
+                       # terms along each dimension
+
+
+# Now lets generate different input parameters and hence files based on halton sequnces
+
+for j in range(n_sample):
+    r=hs[j] #gives 1d array of 5 independent halton terms
+    chla=0.01+r[0]*(10-0.01)# chla range=[0.01,10]
+    theta0=0+r[1]*(85-0)#theta0 range=[0,85]
+    tau550=0+r[2]*(0.4-0)#tau550 range=[0,0.4]
+    irh=np.rint(1+r[3]*(5-1))#irh range=[1,5]
+    iaerosol=np.rint(11+r[4]*(20-11))#iaerosol range=[11,20]
+    filestrbase='chla%05.2f' % chla \
+               +'iaerosol%d' % iaerosol \
+               +'irh%d' % irh \
+               +'theta0%05.2f' %theta0 \
+               +'tau550%05.2f' %tau550
+    input_writer(filestrbase,chla,iaerosol,irh,theta0,tau550)
+#
+#
+
+
+
+
